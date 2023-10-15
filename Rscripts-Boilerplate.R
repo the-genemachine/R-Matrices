10caeb5b75d52255fbaaaa2f498a52d514cf3f66
the-genemachine <gene.gallant@rogers.com>
2023-10-15 14:36:18 -0400

    sdded some functionality

--- Rscripts-Boilerplate.R
+++ Rscripts-Boilerplate.R
@@ -1,13 +1,12 @@
 # -----------------------------------------------------------------------------
 # R Script Boilerplate
-# Author: [Your Name]
-# Date: [Date]
-# Description: [Short description of what the script does]
+# Author: Gene Gallant
+# Date: October 14th, 2023
+# Description: A template for R scripts
 # -----------------------------------------------------------------------------
 
-# 1. Clear the environment and console
-rm(list = ls())
-cat("\f")
+# 1. Clear the console
+cat("\033c")
 
 # 2. Set options for better debugging and reproducibility
 options(stringsAsFactors = FALSE)  # avoid strings being converted to factors
@@ -18,23 +17,24 @@ options(error = recover)           # provides menu to jump to error location
 
 # 4. Load required libraries/packages
 # If you don't have these installed, install using install.packages('package_name')
-library(tidyverse)  # for data manipulation and visualization
+# library(tidyverse)  # for data manipulation and visualization
 
-# 5. Custom functions (if any)
-example_function <- function(x) {
-  # Some code here
-  return(x)
+display_title <- function(title) {
+  border_length <- nchar(title) + 4  # Add 4 for spacing
+  double_border <- paste0(rep("=", border_length), collapse = "")
+  cat(double_border, "\n")
+  cat("= ", title, " =\n", sep = "")
+  cat(double_border, "\n\n")
 }
-
 # 6. Main code
 # Load data, perform analysis, etc.
 # data <- read.csv("data.csv")
-
+display_title("Data")
 # 7. Save results (if required)
 # saveRDS(result, "results.RDS")
 
 # 8. End of script message
-message("Script completed!")
+# message("Script completed!")
 
 # -----------------------------------------------------------------------------
 # End of Script
